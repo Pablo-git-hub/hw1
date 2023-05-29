161ea4b7580e776061e38d12bbb4a6a967a2c76f
Pablo-git-hub <128069796+Pablo-git-hub@users.noreply.github.com>
2023-05-29 23:19:01 +0200

    Update preferiti.php

--- preferiti.php
+++ preferiti.php
@@ -1,46 +1,58 @@
 
-
 <?php
-    require_once 'auth.php';
-    if (!$userid = checkAuth()) {
-        header("Location: login_session.php");
-        exit;
-    }
+require_once 'auth.php';
+if (!$userid = checkAuth()) {
+    header("Location: login_session.php");
+    exit;
+}
 
 $conn = mysqli_connect($dbconfig['host'], $dbconfig['user'], $dbconfig['password'], $dbconfig['name']);
 
 // Controlla la connessione al database
-  if (!$conn) {
-        die("Connessione al database fallita: " . mysqli_connect_error());
-    }
+if (!$conn) {
+    die("Connessione al database fallita: " . mysqli_connect_error());
+}
+
 // Inizializza una sessione (assicurati di chiamare questa funzione all'inizio dello script)
-
-
 
 // Ottieni i dati inviati tramite la richiesta AJAX
 $data = json_decode(file_get_contents('php://input'), true);
 
 // Prepara i dati per l'inserimento nella tabella dei preferiti
-if($data){
- $name = mysqli_real_escape_string($conn, $data['name']);
-$image = mysqli_real_escape_string($conn, $data['image']);
-$userid = mysqli_real_escape_string($conn, $_SESSION['session_user_id']);
-  if($name!=null and $image!=null){
-    // Esegui l'inserimento nella tabella dei preferiti
-$sql = "INSERT IGNORE INTO preferiti (name, image, user) VALUES ('$name', '$image', '$userid') ";
+if ($data) {
+    $name = mysqli_real_escape_string($conn, $data['name']);
+    $image = mysqli_real_escape_string($conn, $data['image']);
+    $userid = mysqli_real_escape_string($conn, $_SESSION['session_user_id']);
 
-if ($conn->query($sql) === TRUE) {
-    // Inserimento riuscito
-    echo "Dati salvati correttamente nel database.";
-} else {
-    // Errore durante l'inserimento
-    echo "Errore durante il salvataggio dei dati nel database: " . $conn->error;
+    if ($name != null && $image != null) {
+        // Esegui la query per verificare se esiste già una riga con i valori specificati
+        $checkQuery = "SELECT * FROM preferiti WHERE name = '$name' AND image = '$image' AND user = '$userid'";
+        $checkResult = mysqli_query($conn, $checkQuery);
+
+        if (!$checkResult) {
+            die("Errore nella query: " . mysqli_error($conn));
+        }
+
+        // Controlla il numero di righe restituite dalla query
+        if (mysqli_num_rows($checkResult) > 0) {
+            // La riga duplicata esiste già nella tabella
+            echo "La riga esiste già nella tabella dei preferiti.";
+        } else {
+            // Esegui l'inserimento nella tabella dei preferiti
+            $insertQuery = "INSERT INTO preferiti (name, image, user) VALUES ('$name', '$image', '$userid')";
+            if (mysqli_query($conn, $insertQuery)) {
+                // Inserimento riuscito
+                echo "Dati salvati correttamente nel database.";
+            } else {
+                // Errore durante l'inserimento
+                echo "Errore durante il salvataggio dei dati nel database: " . mysqli_error($conn);
+            }
+        }
+
+        // Chiudi il risultato della query
+        mysqli_free_result($checkResult);
+    }
 }
- 
-}
-
-
-  }
 
 // Chiudi la connessione al database
 
